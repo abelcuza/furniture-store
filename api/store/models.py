915c51452905dfc30970cf4c1f77f5c50f0c8d05
Abel Cuza <abel.cuza07@gmail.com>
2023-11-30 14:35:46 -0500

    Add the url for images in product and str method for categories

--- api/store/models.py
+++ api/store/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
 
 class Product(models.Model):
     name = models.CharField(max_length=50)
@@ -11,6 +14,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=8, decimal_places=2)
     stock_quantity = models.IntegerField()
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    img_url = models.CharField(max_length=255)
 
     def __str__(self):
         return self.name
