f71d2eea9aec75c6dc546b5b03688fb36622aaa4
Abel Cuza <abel.cuza07@gmail.com>
2023-11-30 15:12:29 -0500

    Add context for index view

--- api/store/views.py
+++ api/store/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 
+from .models import Product
+
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html', context={})
+    context = {
+        'living_room': Product.objects.filter(category__name="Living Room"),
+        'home_office': Product.objects.filter(category__name="Home Office")
+    }
+    return render(request, 'index.html', context=context)
