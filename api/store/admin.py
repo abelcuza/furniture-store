15b57f496bd9c11c430f79dd3c544eb5299373fd
Abel Cuza <abel.cuza07@gmail.com>
2023-11-30 14:36:01 -0500

    Add models in the admin page

--- api/store/admin.py
+++ api/store/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from api.store.models import *
+
+admin.site.register(Product)
+admin.site.register(Category)
+admin.site.register(Customer)
+admin.site.register(Order)
